29002cca297ec40aadaea148be9762f2b342d8c9
Philip Tam <philiptam@macbook-pro.local>
2018-02-08 01:05:26 -0500

    added a Edit + Delete toggle button to delete cells!

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,10 +40,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
     func applicationWillTerminate(_ application: UIApplication) {
-        print("applicationWillTerminate")
+         print("applicationWillTerminate")
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
+    }
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        // Create a new container of the type NSPersistentContainer of the Structure "DataModel"
+        let container = NSPersistentContainer(name: "DataModel")
+        
+        //Where we load the persistent store and get it ready for use
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+               
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 
 
--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -7,21 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-    // Object that provides a convenient interface to the contents of the File System / Shared File Manager Object = Singleton //userDomainMask = where we install the Users Personal Items
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    // UIApplication.shared.delegate as! AppDelegate goes into the AppDelegate and grabs the persistent container
+    // Where from there we grav the context of the persistent container
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         print(dataFilePath)
         
-        loadItems()
+        loadItems() //(Read)
+        navigationItem.leftBarButtonItem = editButtonItem
     }
 
     //MARK - Tableview Datasourcew Methods
@@ -51,16 +55,31 @@ class TodoListViewController: UITableViewController {
     
     //MARK - TableView Delagate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        
+       // itemArray[indexPath.row].setValue("Completed", forKey: title) Sets the value of title to = "Completed"
+        
         itemArray[indexPath.row].checkmark = !itemArray[indexPath.row].checkmark
         // ! is reverses the value of the .checkmark
         
-        saveItems()
+        saveItems() // (Updates)
         
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
-    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete){
+            context.delete(itemArray[indexPath.row]) // This does nothing to the actual Database because it has to be saved to the Database therefore context.save() must be used to update the Database after
+            
+            itemArray.remove(at: indexPath.row)
+                    //This does nothing to the Core Data because it merely updates our itemArray which is used to populate our tableView so that when we reload it were able to reload the freshest items! This should be done after because of the item at the indexPath.row will be gone and cannot be deleted from the context.delete where the context.delete used the itemArray to find the NSManagedObject
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            saveItems()
+        }
+    }
     //MARK - Add New Items
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
        
@@ -71,11 +90,15 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user hits the Add Item button on our UIAlert
             
-            let newItem = Item()
-            newItem.title = textField.text!
+            //Create new object of type Item that has all access to all the properties that were specified as attributes: checkmark and title. That Item is an object of type NSManagedObject (A.K.A they are the rows inside the tables where every row is an individual NSManagedObject)
+            let newItem = Item(context: self.context)
             
+            //Following the creation it is necessary to fill out the fields! (Create)
+            newItem.title = textField.text!
+            newItem.checkmark = false
             self.itemArray.append(newItem)
             
+            //Following all that we save our Item!
             self.saveItems()
             
             //We must stop using UserDefaults because of it cannot save our own custom data type Item! Therefore antother solution must be found at all cost! UserDefaults can only take standard data! Also it is not a database!
@@ -90,27 +113,27 @@ class TodoListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    func saveItems(){ // Encodes the data
-        let encoder = PropertyListEncoder()
+    func saveItems(){ // Saves the Data from the Context to the Persistent Storage (Create)
         do{
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: dataFilePath!)
+           try context.save()
+            // It looks at the context and saves the uncommited changes towards the Persistent Storage
         }
         catch {
-            print("Error encoding item array \(error)")
+            print("Error saving context \(error)")
         }
         //Reloard the data of the tableview
         tableView.reloadData()
     }
-    func loadItems(){ //Decodes the data
-        if let data = try? Data(contentsOf: dataFilePath!){
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            }
-            catch{
-                print("Error decoding item array \(error)")
-            }
+    func loadItems(){ //Send fetch request to the context where they grab the Data from the Persistent Storage (Read)
+        
+        // You have to specify the Data Type of the Outpute and the Entity you are trying to request!
+        // Array of Items (DATA TYPE) that was stored in the Persistent Storage
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        }
+        catch{
+            print("Error fetching Data from context \(error)")
         }
     }
 }
